17a7348dd9687774c0b4c7b4d442d31c1ebf9cf1
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-18 19:18:54 -0400

    completed

--- js/app.js
+++ js/app.js
@@ -33,21 +33,43 @@ $(function () {
 
   function search (searchTerm) {
     const url = 'https://api.giphy.com/v1/gifs/search'
-    const apiKey = '[ADD YOUR GIPHY API KEY HERE]'
+    const apiKey = 'HRuknNTOGG0i1qVagcgOpKaxQz2OTAop'
 
-    $.ajax({
-      url: url,
-      type: 'GET',
-      data: { q: searchTerm, limit: 50, api_key: apiKey }
-    })
-    .done((response) => {
-      // execute this function if request is successful
-      console.log(response)
-      displayResults(response.data)
-    })
-    .fail(() => {
-      // execute this function if request fails
-      alert('error occurred')
-    })
+
+// adding promises with axios
+// writing the params as an object
+
+axios.get(url, {
+params: {
+  api_key: apiKey,
+  q: searchTerm,
+  limit: 50,
+}
+// the arrow function in then is a callback
+// callback = function passed as a parameter
+// response is not a variable, its a placeholder for the object within the function
+// always make sure to look at the API delivery and naming to call the right details
+}).then((response) => {
+console.log(response.data.data)
+displayResults(response.data.data)
+})
+.catch((e) => {
+  console,log(e)
+})
+
+    // $.ajax({
+    //   url: url,
+    //   type: 'GET',
+    //   data: { q: searchTerm, limit: 50, api_key: apiKey }
+    // })
+    // .done((response) => {
+    //   // execute this function if request is successful
+    //   console.log(response)
+    //   displayResults(response.data)
+    // })
+    // .fail(() => {
+    //   // execute this function if request fails
+    //   alert('error occurred')
+    // })
   }
 })
